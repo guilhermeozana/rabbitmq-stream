37c1d8718e2a4a72bb7619a56a3358a38e063706
guilhermeozana@hotmail.com <guilherme@agide.com.br>
2024-02-05 10:52:10 -0300

    add project structure

---
+++ src/main/java/com/rabbitmqstream/RabbitmqStreamApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmqstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbitmqstream/RabbitmqStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmqstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
